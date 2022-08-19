8434d379ae301734d57b8cae1a081f5b827f6a47
Victor Marxen <victor.marxen@hotmail.de>
2022-08-19 12:27:39 +0100

    Update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
+  before_action :set_list, only: %i[show destroy]
 
   def index
     @lists = List.all
@@ -20,6 +20,11 @@ class ListsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
